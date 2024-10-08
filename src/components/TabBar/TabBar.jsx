2c548b3013b7e7ead0bea6ba79494a7e14c15517
kumar <jyotishgehlot@gmail.com>
2024-10-08 17:04:37 +0530

    Create tab-bar

--- src/components/TabBar/TabBar.jsx
+++ src/components/TabBar/TabBar.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./tabBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBagShopping,
+  faCartShopping,
+  faHandHoldingHand,
+  faHome,
+  faLayerGroup,
+} from "@fortawesome/free-solid-svg-icons";
 
 const TabBar = () => {
+  const [activeTab, setActiveTab] = useState("Home");
+  const tabs = [
+    { icon: faHome, label: "Home" },
+    { icon: faLayerGroup, label: "Category" },
+    { icon: faCartShopping, label: "Cart" },
+    { icon: faBagShopping, label: "Orders" },
+    { icon: faHandHoldingHand, label: "Help" },
+  ];
+
   return (
     <>
       <div className="tab-bar">
-        <h1>Tab Bar</h1>
+        <ul role="tab-button">
+          {tabs.map((tab) => (
+            <li
+              onClick={() => setActiveTab(tab.label)}
+              className={activeTab === tab.label && "active"}
+            >
+              <FontAwesomeIcon icon={tab.icon} />
+              <p>{tab.label}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
