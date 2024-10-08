5d6b86e1695abba5619e6c06893995c0e8369a67
kumar <jyotishgehlot@gmail.com>
2024-10-08 11:19:08 +0530

    set image optimization on product Item
    show placeholder image when it's not loaded

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,29 +2,44 @@ import { useContext, useState } from "react";
 import "./productItem.css";
 import StoreContext from "../../context/StoreContext";
 //font - awesome
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import { placeholderSrc } from "../../assets/assests";
 
 export const ProductItem = ({ id, image, name, unit, price, quantity }) => {
   const { cartItem, addToCart, removeToCart } = useContext(StoreContext);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <div className="image-wrapper">
-      <img src={image} alt={name} loading="lazy" />
+      {(!isLoaded)&&(
+        <img src={placeholderSrc} alt={name} loading="lazy" />  
+      )}
+      <img src={image} alt={name} loading="lazy" style={{opacity:isLoaded?1:0, transition:"opacity 0.5s ease-in-out" }} onLoad={()=>setIsLoaded(true)} />
       <p>{name} asdfsd sd sa fs saf</p>
       <p className="quantity">{quantity + " " + unit}</p>
       <div className="price-box">
         <p className="price">{price}</p>
         <span className="product-item-counter">
           {!cartItem[id] ? (
-            // <div className="firstAddItem" onClick={() => addToCart(id)}>
-              <FontAwesomeIcon icon={faPlus} className="fa-solid fa-plus" onClick={() => addToCart(id)} />
-            // </div>
+            <FontAwesomeIcon
+              icon={faPlus}
+              className="fa-solid fa-plus"
+              onClick={() => addToCart(id)}
+            />
           ) : (
             <div className="count-box">
-              <FontAwesomeIcon icon={faMinus} className="remove-item" onClick={() => removeToCart(id)} />
-              <span className="count-text">{cartItem[id]}</span> 
-              <FontAwesomeIcon icon={faPlus} className="add-item" onClick={() => addToCart(id)} />
+              <FontAwesomeIcon
+                icon={faMinus}
+                className="remove-item"
+                onClick={() => removeToCart(id)}
+              />
+              <span className="count-text">{cartItem[id]}</span>
+              <FontAwesomeIcon
+                icon={faPlus}
+                className="add-item"
+                onClick={() => addToCart(id)}
+              />
             </div>
           )}
         </span>
